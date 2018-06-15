21c1e7052f53ced8e2b78d511f7a4ee34634f0da
Morris <morris@EZ-C02S58DMGG7L.local>
2018-06-15 14:44:11 -0400

    config time to wait after cat detected

--- src/providers/scale-data/scale-data.ts
+++ src/providers/scale-data/scale-data.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
-import { ChangeDetectorRef } from '@angular/core';
 /*
   Generated class for the ScaleDataProvider provider.
 
@@ -28,8 +27,13 @@ export class ScaleDataProvider {
 
   public demoVariables:any={"cat":false,"urine":false,"scoop":false}
 
+  public secondsAfterCat:number=4;
+
+  public urineAllowed:boolean=false;
+
   resetVars(){
     this.demoVariables = {"cat":false,"urine":false,"scoop":false};
+    this.urineAllowed = false;
   }
 
   constructor(private storage: Storage
@@ -45,10 +49,10 @@ export class ScaleDataProvider {
         
         that.catWeightThreshold = parseFloat(data.catWeightThreshold);
         that.urineWeightThreshold=parseFloat(data.urineWeightThreshold);
-        that.scoopWeightThreshold = parseFloat(data.scoopWeightThreshold)
+        that.scoopWeightThreshold = parseFloat(data.scoopWeightThreshold);
+        that.secondsAfterCat = parseFloat(data.secondsAfterCat);
         that.waitInterval = parseInt(data.waitInterval);
         
-
       }
 
 
@@ -72,17 +76,26 @@ export class ScaleDataProvider {
             if (that.demoVariables.cat==that.demoVariables.urine && that.demoVariables.urine==that.demoVariables.scoop ){
               that.catDetected=true;
               that.demoVariables.cat = true;
+
+
             }
 
           }
         }
 
-      // If last 9 values were below cat threshold, then assume cat present
+      // If last 9 values were below cat threshold, then assume cat not present
       if (this.weight_values.length>this.waitInterval){
         // console.log(this.weight_values.slice(this.weight_values.length-9,this.weight_values.length))
           if (this.weight_values.slice(this.weight_values.length-this.waitInterval,this.weight_values.length).every(function(weight){
             return weight<that.catWeightThreshold;
           })){
+            // If we just switched from true to false, than start the timeout
+            if (that.catDetected){
+              // Trigger urine allowed only after a few seconds
+              setTimeout(function(){
+                that.urineAllowed = true
+              }, that.secondsAfterCat*1000)
+            }
             that.catDetected=false;
           }
         }
@@ -95,10 +108,11 @@ export class ScaleDataProvider {
         return weight>that.urineWeightThreshold && weight<that.catWeightThreshold;
       })){
         //console.log("Urine detected!!");
-        // Urine can only change if we observed cat already, and didnt observe scoop yet
-        if (that.demoVariables.cat && !that.demoVariables.scoop ){ 
+        // Urine can only change if we observed cat already, and didnt observe scoop yet, and enough time after cat
+        if (that.demoVariables.cat && !that.demoVariables.scoop && that.urineAllowed){ 
           that.urineDetected=true;
           that.demoVariables.urine = true;
+          
         }
        
       }
@@ -163,7 +177,8 @@ export class ScaleDataProvider {
     var data = {"catWeightThreshold":this.catWeightThreshold,
                 "urineWeightThreshold":this.urineWeightThreshold,
                 "scoopWeightThreshold":this.scoopWeightThreshold.toString(),
-                "waitInterval":this.waitInterval}
+                "waitInterval":this.waitInterval,
+              "secondsAfterCat":this.secondsAfterCat}
 
     
     this.storage.set('data',data);
